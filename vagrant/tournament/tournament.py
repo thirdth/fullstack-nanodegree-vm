393dd72a3e771c6c69914983981065dd6578c7ff
thirdth <zackglaser@gmail.com>
2017-06-21 17:13:02 -0500

    Initial Changes

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
 
+DBNAME = "tournament"
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor
     return psycopg2.connect("dbname=tournament")
 
 
@@ -25,10 +28,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +59,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +76,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,23 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE if exists tournament;
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE players (
+  id serial primary key,
+  name varchar(50) NOT NULL
+);
+
+CREATE TABLE matches (
+  id serial primary key,
+  p_one_id integer references players(id),
+  p_two_id integer references players(id),
+  winner_id integer references players(id),
+  round_num integer
+);
+
+select * from players, matches;
